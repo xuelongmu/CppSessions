04f5f41e2b36282eb766937dd0d7b9549e159aed
xuelongmu <xuelongmu@gmail.com>
2022-03-10 15:44:25 -0500

    added standalone script

--- start-standalone.bat
+++
@@ -1,3 +0,0 @@
-set UE4_BIN=C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\Win64
-
-start /D "%UE4_BIN%" UE4Editor.exe "%cd%\CppSessions.uproject" -game -log
---
+++ start-standalone.ps1
@@ -0,0 +1,3 @@
+C:\Program` Files\Epic` Games\UE_4.27\Engine\Build\BatchFiles\Build.bat CppSessionsEditor Win64 DebugGame "$pwd\CppSessions.uproject" -waitmutex
+C:\Program` Files\Epic` Games\UE_4.27\Engine\Build\BatchFiles\Build.bat CppSessions Win64 DebugGame "$pwd\CppSessions.uproject" -waitmutex
+C:\Program` Files\Epic` Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe "$pwd\CppSessions.uproject" -game -log
